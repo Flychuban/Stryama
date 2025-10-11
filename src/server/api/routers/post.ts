954420d11fbf3d6d10342621a6e0c71b3bc53537
Flychuban <kaloanas07@gmail.com>
2025-10-11 13:07:40 +0300

    add code for story 1.3

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,4 +1,4 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter } from '~/server/api/trpc';
 
 export const postRouter = createTRPCRouter({
   // Your routes will go here
